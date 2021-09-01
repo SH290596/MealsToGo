a896d9355be6c8a28b2838813609287f439a5d2d
Stephen Henry <stephenhenry@prime19.ie>
2021-09-01 17:42:05 +0100

    Using SVG with Ratings part of Course

--- App.js
+++ App.js
@@ -1,24 +1,32 @@
-import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import React from 'react';
-import { StatusBar, StyleSheet, SafeAreaView, Text, View, Platform } from 'react-native';
-
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { RestaurantsScreen } from "./src/features/restaurants/Screens/restaurants.screen";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
-      <SafeAreaView style={{ flex:1, marginTop: StatusBar.currentHeight}}>
-        <View style={{padding: 16, backgroundColor: "green"}}>
-          <Text>search</Text>
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue"}}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
-      <ExpoStatusBar style="auto"/>
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-
-});
---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Text, View } from "react-native";
+import { Card } from "react-native-paper";
+
+// Used to style the components within the restaurant card function
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled(View)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+// Used to build out the restaurant card segment
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.sportsfile.com/winshare/w540/Library/SF264/880693.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Title> {name} </Title>
+        <Address> {address} </Address>
+      </Info>
+    </RestaurantCard>
+  );
+};
